d82f64b85f10ddf69010c6e23f092d82937a1db4
andrewscarlos <andrews.knct@gmail.com>
2024-01-15 15:43:59 -0300

    fix: cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_VERCEL_URL || '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept' },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["s2.coinmarketcap.com"],
