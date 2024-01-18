7bd77b008ecf5c877bddae1ba23dc39d87803b32
andrewscarlos <andrews.knct@gmail.com>
2024-01-18 11:37:23 -0300

    feat: remove babel and generate a new build

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  };
-  
